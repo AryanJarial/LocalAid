79829e73c1ab2fa04931e876b0a60bc899231f1c
Aryan Jarial <aryanjarial77@gmail.com>
2026-01-01 19:50:44 +0530

    Add user login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 
 const registerUser = async (req, res) => {
@@ -24,6 +25,7 @@ const registerUser = async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        token: generateToken(user._id),
       });
     } else {
       res.status(400).json({ message: 'Invalid user data' });
@@ -33,4 +35,26 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+const authUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { registerUser, authUser };
