480804991f7c9b804e577b879fbc956c89cf5b16
Aryan Jarial <aryanjarial77@gmail.com>
2026-01-01 19:13:25 +0530

    Mount user registration routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(express.json()); 
 app.use(cors());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
