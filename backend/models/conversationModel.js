0306c859cf56d52b3c95df2951eac1a9c4eeeb02
Aryan Jarial <aryanjarial77@gmail.com>
2026-01-11 11:18:09 +0530

    add conversation and message model

---
+++ backend/models/conversationModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const conversationSchema = mongoose.Schema(
+  {
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message',
+    },
+  },
+  { timestamps: true }
+);
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+export default Conversation;
